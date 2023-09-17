36a325f4c6b89d2445dd20ea491af784155cd042
SamikshaD1 <127653626+SamikshaD1@users.noreply.github.com>
2023-09-17 21:19:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 function getFormvalue() {
     //Write your code here
+const firstName = document.querySelector(".fname").value;
+const lastName = document.querySelector(".lname").value;
 
+	alert(firstName + lastName);
 }
